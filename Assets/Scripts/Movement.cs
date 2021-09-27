83d4e69a63ca490bc847dea4db3b0f5c9731e5cc
Amirkhan <amirkhan13101998@gmail.com>
2021-09-27 18:05:20 +0600

    DoTween added. Bottle stuck.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,11 +1,35 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private bool putToRight = true;
+    private float offset = 0.16f;
+    private int count = 1;
 
     void Update()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bottle"))
+        {
+            other.transform.parent = transform;
+            if (putToRight)
+            {
+                other.transform.DOLocalMove(new Vector3(offset * count, 0, 0), 1f);
+                putToRight = false;
+            }
+            else
+            {
+                other.transform.DOLocalMove(new Vector3(-offset * count, 0, 0), 1f);
+                putToRight = true;
+                count++;
+            }
+        }
     }
 }
