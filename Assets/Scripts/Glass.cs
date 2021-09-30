fec3249db827c4390dcba0ce2296d794e15ae9a5
Amirkhan <amirkhan13101998@gmail.com>
2021-10-01 01:13:18 +0600

    Filling winglass fixed.

--- Assets/Scripts/Glass.cs
+++ Assets/Scripts/Glass.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 public class Glass : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other)
+    private void OnTriggerEnter(Collider other)
     {
-        GetComponent<Collider>().enabled = false;
-        transform.GetChild(0).gameObject.SetActive(true);
-        transform.DOMoveY(transform.position.y + 0.1f, 0.1f)
-            .OnComplete(() =>
-                transform.DOScale(transform.localScale * 1.5f, 0.2f)
-                    .OnComplete(() => transform.DOScale(transform.localScale / 1.5f, 0.2f)
-                        .OnComplete(() => { transform.DOMoveY(transform.position.y - 0.1f, 0.1f); }))
-            );
+        if (other.gameObject.CompareTag("Bottle"))
+        {
+            GetComponent<Collider>().enabled = false;
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.DOMoveY(transform.position.y + 0.1f, 0.1f)
+                .OnComplete(() =>
+                    transform.DOScale(transform.localScale * 1.5f, 0.2f)
+                        .OnComplete(() => transform.DOScale(transform.localScale / 1.5f, 0.2f)
+                            .OnComplete(() => { transform.DOMoveY(transform.position.y - 0.1f, 0.1f); }))
+                );
+        }
     }
 }
