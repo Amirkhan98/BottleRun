142b5dbbea72ef73068173cd0b5cecb5b3d41f32
Amirkhan <amirkhan13101998@gmail.com>
2021-10-08 01:02:05 +0600

    Changing painting system from paint3d to pooling system.

--- Assets/Effect.cs
+++ Assets/Effect.cs
@@ -1,23 +1,62 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using PathologicalGames;
 using UnityEngine;
 
 public class Effect : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem particles;
+    private SpawnPool shapesPool;
+    [SerializeField] private int spawnAmount = 20;
+    private string poolName = "Liquid";
+    [SerializeField] private GameObject circleSprite;
+    [SerializeField] private float spawnInterval;
 
-    void Start()
+    private void Start()
     {
+        shapesPool = PoolManager.Pools[poolName];
     }
 
-    private void OnParticleCollision(GameObject other)
+    private void OnEnable()
     {
-        
+        StartCoroutine("Spawner");
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Spawner()
     {
+        int count = spawnAmount;
+        Transform inst;
+        SpawnPool shapesPool = PoolManager.Pools[poolName];
+        while (count > 0)
+        {
+            // Spawn in a line, just for fun
+            inst = shapesPool.Spawn(circleSprite);
+            inst.localPosition = new Vector3((spawnAmount + 2) - count, 0, 0);
+            count--;
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+        StartCoroutine(Despawner());
+
+        yield return null;
+    }
+
+
+    private IEnumerator Despawner()
+    {
+        var spawnedCopy = new List<Transform>(shapesPool);
+        Debug.Log(shapesPool.ToString());
+        foreach (Transform instance in spawnedCopy)
+        {
+            shapesPool.Despawn(instance); // Internal count--
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+        // Restart
+        StartCoroutine(Spawner());
+
+        yield return null;
     }
 }
---
+++ Assets/Scripts/Cup.cs
@@ -0,0 +1,31 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+    [SerializeField] private GameObject liquid;
+    [SerializeField] private GameObject particles;
+    [SerializeField] private Transform surface;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bottle"))
+        {
+            PlayerController.OnWineGlassFill.Invoke();
+            GetComponent<Collider>().enabled = false;
+            liquid.SetActive(true);
+            liquid.transform.DOLocalMoveY(surface.transform.localPosition.y, 0.5f);
+            liquid.transform.DOScale(surface.transform.localScale, 0.5f).OnComplete(() =>
+            {
+                particles.gameObject.SetActive(true);
+            });
+
+            transform.DOMoveY(transform.position.y + 0.1f, 0.1f)
+                .OnComplete(() =>
+                    transform.DOScale(transform.localScale * 1.3f, 0.2f)
+                        .OnComplete(() => transform.DOScale(transform.localScale / 1.3f, 0.2f)
+                            .OnComplete(() => { transform.DOMoveY(transform.position.y - 0.1f, 0.1f); }))
+                );
+        }
+    }
+}
